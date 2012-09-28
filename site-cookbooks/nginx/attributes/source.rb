0e6fb9dcc310e7e753a3407205bace29a8548c82
maukoquiroga <maukoquiroga@gmail.com>
2012-09-28 18:54:36 -0400

    Revert "Update site-cookbooks/nginx/attributes/source.rb"
    This reverts commit abf83bb734b9de91fb935566e0f2d11b18d71a17.

--- site-cookbooks/nginx/attributes/source.rb
+++ site-cookbooks/nginx/attributes/source.rb
@@ -19,8 +19,6 @@
 # limitations under the License.
 #
 
-default['nginx']['source'] ||= {}
-
 set['nginx']['source']['prefix']                  = "/opt/nginx-#{node['nginx']['version']}"
 set['nginx']['source']['conf_path']               = "#{node['nginx']['dir']}/nginx.conf"
 set['nginx']['source']['default_configure_flags'] = [
